a45d8f86e33d87f6caec14e323d393ab70419682
Ridho DL <107058352+nvdstore@users.noreply.github.com>
2025-04-17 23:31:40 +0700

    Update ThemeToggle.js

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -53,7 +53,7 @@ const ThemeToggle = () => {
         href="https://www.nvdtopup.web.id?utm_source=news&utm_campaign=redirect-button"
         target="_blank"
         rel="noopener noreferrer"
-        className="md:flex flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-background text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 gap-2"
+        className="md:flex flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 gap-2"
       >
         <Image
           src="/images/fire.gif"
