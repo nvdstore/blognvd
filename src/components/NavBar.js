ae0cb46cce500b95ecc6fe30f36bbb4996d505f5
Ridho DL <107058352+nvdstore@users.noreply.github.com>
2025-04-17 23:14:55 +0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -99,6 +99,22 @@ const NavBar = ({ className }) => {
                 </NavigationMenuItem>
               </NavigationMenuList>
             </NavigationMenu>
+
+                 <a
+    href="https://www.takapedia.com?utm_source=news&utm_campaign=redirect-button"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="md:flex flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 gap-2"
+  >
+    <Image
+      src="/_next/static/media/fire.3dc720a7.gif"
+      alt="Top Up Icon"
+      width={20}
+      height={20}
+      className="h-5 w-5 object-contain"
+    />
+    Top Up <span className="hidden md:inline">Sekarang</span>
+  </a>
           </div>
 
           <div className="flex items-center gap-4">
