824efbdcb5cb4043152ef93a8f860a3eecd19aa7
Ridho DL <107058352+nvdstore@users.noreply.github.com>
2025-04-17 23:29:33 +0700

    Update ThemeToggle.js

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -56,7 +56,7 @@ const ThemeToggle = () => {
         className="md:flex flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-background text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 gap-2"
       >
         <Image
-          src="/images/logonvd.gif"
+          src="/images/fire.gif"
           alt="Top Up Icon"
           width={20}
           height={20}
